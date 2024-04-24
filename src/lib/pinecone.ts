10f17cc4058d0e9b7ce1ac2043144851cbe20597
keonilna-onycom <keonilna@onycom.com>
2024-04-24 15:07:31 +0900

    feat: update pinecone

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -1,12 +1,20 @@
-import { PineconeClient } from '@pinecone-database/pinecone'
+import { Pinecone } from "@pinecone-database/pinecone";
 
 export const getPineconeClient = async () => {
-  const client = new PineconeClient()
-
-  await client.init({
+  const pc = new Pinecone({
     apiKey: process.env.PINECONE_API_KEY!,
-    environment: 'us-east1-gcp',
-  })
+  });
 
-  return client
-}
+  await pc.createIndex({
+    name: "quill",
+    dimension: 1536,
+    metric: "cosine",
+    spec: {
+      serverless: {
+        cloud: "aws",
+        region: "us-east-1",
+      },
+    },
+  });
+  return pc;
+};
